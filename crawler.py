29877dec8692080d04adbf570fdccd179abf33b1
rlgehring89 <33910567+rlgehring89@users.noreply.github.com>
2019-12-14 16:41:42 -0500

    Finished function to filter by warehouse location

--- crawler.py
+++ crawler.py
@@ -29,8 +29,9 @@ def setup_driver (headless,browser,implicitly_wait):
 	else:
 		driver = webdriver.Firefox()
 	
+	actions = ActionChains(driver)
 	driver.implicitly_wait(15)
-	return driver
+	return driver,actions
 
 def filter_auctions_by_zip(driver,bid_fta_homepage,zip_code):
 	#Wait action Timeout explicit when needed
@@ -52,28 +53,66 @@ def filter_auctions_by_zip(driver,bid_fta_homepage,zip_code):
 	wait.until(EC.invisibility_of_element_located(loadingOverlay))
 	time.sleep(2)
 
-	#Print all auction results on page
-	results = driver.find_elements_by_xpath("//*[@class='col-xs-12 col-sm-6 col-md-4 col-lg-3 product-list padd-0 slick-slide slick-active']")
-	print("==========================================================================")
-	for eachAuction in results:
-		print(eachAuction.text)
 
-def get_all_auctions_filtered_locations(driver,bid_fta_all_auctions,city):
 
-	#From dropdown select anything with cincinnati
+
+def filter_all_auctions_page_by_warehouses_in_city(driver,bid_fta_all_auctions,city):
+	wait = WebDriverWait(driver, 10)
+
+	#Open the Webpage
 	driver.get(bid_fta_all_auctions)
+
+	#Expand the warehouse location dropdown
+	clickable_warehouse_dropdown = driver.find_element_by_xpath("//*[@class='multiselect dropdown-toggle btn btn-default']")
+	clickable_warehouse_dropdown.click()
+
+	#Find all the dropdown options
 	warehouse_dropdown = driver.find_element_by_xpath("//*[@class='multiselect-container dropdown-menu']")
 	warehouses = warehouse_dropdown.find_elements_by_tag_name("li")
-	print("==========================================================================")
+
+	#Loop through the warehouse locations for any containing the city of interest and select them
 	for warehouse in warehouses:
 		warehouse_location = warehouse.find_element_by_class_name("checkbox")
-		print(warehouse_location.get_attribute("title"))
+		warehouse_location_name = warehouse_location.get_attribute("title")
+		if city in warehouse_location_name:
+			#Checks that the warehouse location isnt already selected since we dont want it to de-select it
+			warehouse_location_classes = warehouse.get_attribute("class")
+			if "active" not in warehouse_location_classes:
+				warehouse.click()
+	
+	#Click the filter button
+	filter_button = driver.find_element_by_xpath("//*[@class='btn btn-lg btn-style filter-btn']")
+	filter_button.click()
+
+	#Wait for loading overlay
+	loadingOverlay = driver.find_element_by_class_name("overlay")
+	wait.until(EC.invisibility_of_element_located(loadingOverlay))
+	time.sleep(2)
+
+def get_all_auctions_on_page (driver):
+	wait = WebDriverWait(driver, 10)
+	#Print all auction results on page bidfta.com/home
+	all_auctions = driver.find_elements_by_xpath("//div[starts-with(@id,'auctionContainer')]")
+	for each_auction in all_auctions:
+		print(each_auction.text)
+
+	#This gets auctions on the bidfta.com when filtering by zipcode... doesnt current work right.
+	'''
+	results = driver.find_elements_by_xpath("//div[contains(@class,'product-list padd-0 slick-slide')]") #product-list padd-0 slick-slide
+	
+	print(results)
+	print("==========================================================================")
+	for eachAuction in results:
+		print(eachAuction.get_attribute("aria-describedby"))
+		print(eachAuction.text)
+	'''
 
 def clean_up ():
 	driver.quit()
 
 #Run it
-driver = setup_driver (headless,browser,implicitly_wait)
-get_all_auctions_filtered_locations(driver,bid_fta_all_auctions,city)
-#filter_auctions_by_zip(driver,bid_fta_homepage)
+driver,actions = setup_driver (headless,browser,implicitly_wait)
+filter_all_auctions_page_by_warehouses_in_city(driver,bid_fta_all_auctions,city)
+#filter_auctions_by_zip(driver,bid_fta_homepage,zip_code)
+get_all_auctions_on_page(driver)
 #clean_up()
